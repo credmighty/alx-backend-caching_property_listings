dc6a72a4579a45e73cf678041f8010da7ae38235
Abdullah Abdulrofiu <abdulrofiuabdullahi@gmail.com>
2025-11-02 14:14:58 +0100

    files added: urls, views

--- properties/models.py
+++ properties/models.py
@@ -6,7 +6,7 @@ class Property(models.Model):
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
     location = models.CharField(max_length=100)
-    created_at = models.DateTimeField(autonowadd=True)
+    created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.title
